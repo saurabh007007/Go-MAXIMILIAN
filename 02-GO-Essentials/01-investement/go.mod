7191e74f0f44779629bfd848ac13ec8d8c46e92f
saurabh007007 <loverboybhol143@gmail.com>
2025-06-17 01:09:40 +0530

    (feat add): added the go investemet code

---
+++ 02-GO-Essentials/01-investement/go.mod
@@ -0,0 +1,3 @@
+module example.com/investement
+
+go 1.24.4
---
+++ 02-GO-Essentials/01-investement/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// investement tracker for the personal
+
+func main() {
+	var investementAmount = 1000
+	var expectedReturn = 5.5
+	var years = 10
+	var futureValue = float64(investementAmount) * math.Pow((1+expectedReturn)/100, float64(years))
+	fmt.Printf("The future value of the investement is: %.2f\n", futureValue)
+
+}
+
+// go run .  // it runs the current directory
+// go run main.go  // it runs the main file
