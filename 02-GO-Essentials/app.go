748243a17d13fcfcfd5f0fb552c63aa73a1962b1
saurabh007007 <loverboybhol143@gmail.com>
2025-06-16 23:52:16 +0530

    (fix): added the notes along the

--- 02-GO-Essentials/app.go
+++ 02-GO-Essentials/app.go
@@ -1,8 +1,26 @@
 package main
 
-import "fmt"
-
 // it tell tthat it is the main starting point of the function
-func main() {
-	fmt.Print("Kya haal hai ")
-}
+// func main() {
+// 	fmt.Print("Kya haal hai ")
+// }
+
+// to run the go run file name
+
+// go build  it
+
+// cannot find main module, but found .git/config in /home/saurabh/Desktop/Go-Maximilan
+// to create a module there, run:
+// // cd .. && go mod init
+
+// it  gives the error because it menas a go project it could
+
+// go mod init turn this project into module
+
+// go mod init exaample.com/first-app it create the files
+
+// it generates the first app
+
+// run ./first-app  -- it runs the app without anyone to install the go
+
+// thats the reason that we require packages
