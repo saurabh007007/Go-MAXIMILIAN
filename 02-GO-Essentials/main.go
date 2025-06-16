c09c5a519e4cf52745bd788d9a115dee8b46cc75
saurabh007007 <loverboybhol143@gmail.com>
2025-06-16 23:46:16 +0530

    (feat add): added the main.go

---
+++ 02-GO-Essentials/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt" // part of the build in packages
+
+func main() {
+	fmt.Print("hi there ")
+}
+
+// only doule quote or the ` ` backtics are allowed only
+
+// go divides the applications into packaeges
