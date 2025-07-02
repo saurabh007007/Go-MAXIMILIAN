491e014e945a66e45fc5eeafaa242fcf7f20ab7c
saurabh007007 <loverboybhol143@gmail.com>
2025-07-02 19:11:20 +0530

    basic

--- 05-more-exercis/new.go
+++ 05-more-exercis/new.go
@@ -1,8 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
-}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
+		fmt.Fprintln(w, "Hello, World!")
+	})
 
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "About Page")
+	})
+
+	fmt.Println("Server is running on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
