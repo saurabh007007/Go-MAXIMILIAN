0e2cd86c0a7c0dd2705a03c97e7a493bab38a55d
saurabh007007 <loverboybhol143@gmail.com>
2025-07-02 20:46:04 +0530

    basic

--- 05-more-exercis/new.go
+++ 05-more-exercis/new.go
@@ -19,6 +19,27 @@ func main() {
 		fmt.Fprintln(w, "About Page")
 	})
 
+	http.HandleFunc("/submit", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			w.Header().Set("Content-Type", "text/html")
+			fmt.Fprintln(w, `<form method="POST" action="/submit">`)
+			fmt.Fprintln(w, `Name: <input name="name" type="text" /> <input type="submit" value="Submit" />`)
+			fmt.Fprintln(w, `</form>`)
+			return
+		}
+		if r.Method == http.MethodPost {
+			name := r.FormValue("name")
+			if name == "" {
+				fmt.Fprintln(w, "Please provide a name.")
+				return
+			}
+			fmt.Fprintf(w, "Hello, %s!", name)
+			return
+		}
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintln(w, "Method not allowed.")
+	})
+
 	fmt.Println("Server is running on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
